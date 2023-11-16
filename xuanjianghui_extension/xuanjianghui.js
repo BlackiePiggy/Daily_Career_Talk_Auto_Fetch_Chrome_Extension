3c4d77b56f8617ab7e28eda4189cf107cafbad6a
Jason Lee <63088679+BlackiePiggy@users.noreply.github.com>
2023-11-17 00:29:49 +0800

    解决掉了“遇到‘已取消’报错”的问题；改变了加序号的思路，现在普适性更强；添加了空白院内宣讲会模板

--- xuanjianghui_extension/xuanjianghui.js
+++ xuanjianghui_extension/xuanjianghui.js
@@ -4,11 +4,16 @@ var xjhxxCon = document.getElementById("xjhxxConHtm");
 // 获取所有xjhxxList元素
 var xjhxxListElements = xjhxxCon.getElementsByClassName("xjhxxList");
 
-var userDefinedDate = prompt("请输入日期字符串（例如：11-16）:", "11-16");
+var userDefinedDate = prompt("请输入日期字符串（例如：11-16）:", "XX-XX");
+var userDefinedNumber = prompt("请输入电院宣讲会数量（例如：3）:", "X");
 
-var outputString = "";
+var outputString = "￥【" + userDefinedDate.match(/\d+/g)[0] + "月" + userDefinedDate.match(/\d+/g)[1] + "日" + " 今日宣讲会预告】";
 
-outputString += "【" + userDefinedDate.match(/\d+/g)[0] + "月" + userDefinedDate.match(/\d+/g)[1] + "日" + " 今日宣讲会预告】" + "\n";
+for(let i = 0; i < Number(userDefinedNumber); i++){
+    outputString += "￥【】" + " ";
+    outputString +="宣讲会地点：，";
+    outputString +="宣讲时间：\n";
+}
 
 // 遍历每个xjhxxList元素
 for (var i = 0; i < xjhxxListElements.length; i++) {
@@ -23,24 +28,28 @@ for (var i = 0; i < xjhxxListElements.length; i++) {
     if (dateElement.textContent.trim() == userDefinedDate)
     {
         // 提取xjhxxUnits中的元素
-        var xjhxxUnits = xjhxxList.getElementsByClassName("xjhxxUnits");
-        var xjhxxUnitName = xjhxxUnits[0].getElementsByClassName("xjhxxUnitName")[0].textContent.trim();
-        var xjhdz = xjhxxUnits[0].getElementsByClassName("xjhdz")[0].textContent.trim();
+        var xjhxxStatus = xjhxxList.getElementsByClassName("xjhxxStatus")[0].childNodes[1].childNodes[1].textContent;
 
-        // 提取onclick中的内容
-        var onclickContent = xjhxxList.getAttribute("onclick");
+        if(xjhxxStatus == "未开始"){
+            var xjhxxUnits = xjhxxList.getElementsByClassName("xjhxxUnits");
+            var xjhxxUnitName = xjhxxUnits[0].getElementsByClassName("xjhxxUnitName")[0].textContent.trim();
+            var xjhdz = xjhxxUnits[0].getElementsByClassName("xjhdz")[0].textContent.trim();
 
-        var xjhxxid = onclickContent.match(/\d+/)[0]
+            // 提取onclick中的内容
+            var onclickContent = xjhxxList.getAttribute("onclick");
 
-        // 将提取的信息输出到txt文本中
-        // 你可以根据需要将信息拼接成字符串，然后写入到txt文件中
-        // 这里仅作为演示，具体的写入文件操作可能需要根据你的环境和需求进行调整
-        // 示例：将信息拼接成字符串
-        
-        outputString +="【" + xjhxxUnitName + "】" + " ";
-        outputString +="宣讲会地点：" + xjhdz + "，";
-        outputString +="宣讲时间：" + addTwoHoursToTime(timeElement.textContent.trim()) + "\n";
-        outputString +="招聘链接：" + "https://www.job.sjtu.edu.cn/career/xjhxx/view/" + xjhxxid + "\n\n";
+            var xjhxxid = onclickContent.match(/\d+/)[0]
+
+            // 将提取的信息输出到txt文本中
+            // 你可以根据需要将信息拼接成字符串，然后写入到txt文件中
+            // 这里仅作为演示，具体的写入文件操作可能需要根据你的环境和需求进行调整
+            // 示例：将信息拼接成字符串
+            
+            outputString +="￥【" + xjhxxUnitName + "】" + " ";
+            outputString +="宣讲会地点：" + xjhdz + "，";
+            outputString +="宣讲时间：" + addTwoHoursToTime(timeElement.textContent.trim()) + "\n";
+            outputString +="招聘链接：" + "https://www.job.sjtu.edu.cn/career/xjhxx/view/" + xjhxxid + "\n";
+        }
     }
 }
 
@@ -87,13 +96,13 @@ function formatTime(date) {
 
 function addLineNumbers(inputString) {
   // 将字符串按行分割成数组
-  const lines = inputString.split('\n');
+  const lines = inputString.split('￥');
+
+  lines.shift();
 
   // 遍历数组，从第二行开始添加标号
-  let a = 0;
-  for (let i = 1; i < (lines.length - 1); i=i+3) {
-    a++;
-    lines[i] = String.fromCodePoint(0x245F + a)+`${lines[i]}`;
+  for (let i = 1; i < (lines.length); i++) {
+    lines[i] = String.fromCodePoint(0x245F + i)+`${lines[i]}`;
   }
 
   // 将数组合并成一个字符串
